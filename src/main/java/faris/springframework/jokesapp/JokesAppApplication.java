e5dea481c77542ceb8bbd0298e4149491c5c47c9
farisleh <farisleh1@gmail.com>
2021-06-09 11:38:29 +0800

    Closes #1

---
+++ src/main/java/faris/springframework/jokesapp/JokesAppApplication.java
@@ -0,0 +1,1 @@
+package faris.springframework.jokesapp;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class JokesAppApplication {    public static void main(String[] args) {        SpringApplication.run(JokesAppApplication.class, args);    }}
